64b115f3dba0c486673f617036076dbbd6822b53
JusaBerg-ship-it <62331351+JusaBerg-ship-it@users.noreply.github.com>
2020-03-27 15:17:08 +0200

    Nyt kait toimii

--- Peli_testailua/peli.js
+++ Peli_testailua/peli.js
@@ -22,20 +22,6 @@ function startGame() {
     player = new component(20, 20, "red", 20, 20);
 }
 
-var myGameArea = {
-    canvas : document.createElement("canvas"),
-    start : function() {
-        this.canvas.width = 620;
-        this.canvas.height = 620;
-        this.context = this.canvas.getContext("2d");
-        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-        this.interval = setInterval(updateGameArea, 20);
-    },
-    clear : function() {
-      this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    }
-}
-
 function component(width, height, color, x, y) {
     this.width = width;
     this.height = height;
@@ -52,13 +38,26 @@ function updateGameArea() {
     myGameArea.clear();
     var tileCount = 0;
     tileCount = tilesArray.length;
-    var i = 0; 
-    while(i <= tileCount){
+    var i; 
+    for (i =0; i < tilesArray.length; i++){
         tilesArray[i].update();
         //console.log(tilesArray[i]);
-        i++;
 
     }
     player.update();
  
   }
+  
+  var myGameArea = {
+    canvas : document.createElement("canvas"),
+    start : function() {
+        this.canvas.width = 620;
+        this.canvas.height = 620;
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.interval = setInterval(updateGameArea, 20);
+    },
+    clear : function() {
+      this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+}
