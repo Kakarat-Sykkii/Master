9b0c48f2763fa24864676966d49953a47f6c4a94
JusaBerg-ship-it <62331351+JusaBerg-ship-it@users.noreply.github.com>
2020-04-20 19:39:31 +0300

    ökjv yi

--- Kakarat_sykkii/index.php
+++ Kakarat_sykkii/index.php
@@ -34,10 +34,6 @@ Released   : 20130526
 	<div id="logo">
         <h1><a href="#">Kakarat sykkii</a></h1>
         <div id="login2">
-    <ul>
-        <li><a href="#" accesskey="6" title="" id="login">Log in</a></li>
-        <li><a href="#" accesskey="7" title="" id="register">Register</a></li>
-    </ul>
 </div>
     </div>
         
