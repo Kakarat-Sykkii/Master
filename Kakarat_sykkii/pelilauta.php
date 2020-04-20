2026fb728b95dfc4fd0eb377ded638b8bab038ba
JusaBerg-ship-it <62331351+JusaBerg-ship-it@users.noreply.github.com>
2020-04-20 20:33:14 +0300

    siivous

--- Kakarat_sykkii/pelilauta.php
+++ Kakarat_sykkii/pelilauta.php
@@ -33,7 +33,6 @@ Released   : 20130526
                 <li class="current_page_item"><a href="#" accesskey="2" title="">Pelilauta</a></li>
                 <li><a href="aboutus.php" accesskey="3" title="">Tietoa meistä</a></li>
                 <li><a href="vinkkeja.php" accesskey="4" title="">Vinkkejä liikuntaan</a></li>
-                <li><a href="#" accesskey="5" title="">Contact Us</a></li>
             </ul>
         </div>
     </div>
