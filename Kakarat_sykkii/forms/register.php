ebe94cd8f3b2bf64811e5b38651158b7af012540
ilkkator <62331730+ilkkator@users.noreply.github.com>
2020-05-04 21:43:54 +0300

    asd

--- Kakarat_sykkii/forms/register.php
+++ Kakarat_sykkii/forms/register.php
@@ -53,9 +53,7 @@ if(isset($_POST['submitUser'])){
 ?>
 
 <?php
-if(isset($_SESSION['swarningInput'])){
-  echo("<p class=\"warning\">ILLEGAL INPUT: ". $_SESSION['swarningInput']."</p>");
-}
+
 //Luovutetaanko ja palataan takaisin pääsivulle alkutilanteeseen
 if(isset($_POST['submitBack'])){
   session_unset();
